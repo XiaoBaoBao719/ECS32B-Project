b7088c3bfac3b1a9f515afab0ea710b7bfe44eab
gspiga <78838666+gspiga@users.noreply.github.com>
2021-02-09 21:12:43 -0800

    Update part1_XB.py

--- part1_XB.py
+++ part1_XB.py
@@ -14,7 +14,7 @@ class Truck:
         self.id = id
         self.size = n
         self.location = loc
-        # self.packages = ?
+        self.packages = []
 
 
     def collectPackage(self, pk):
