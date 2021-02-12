157570cf91c54eefa4b6a415c59a17e0b1299ae8
XiaoBaoBao719 <xiao-bao719@hotmail.com>
2021-02-11 16:39:23 -0800

    updated

--- hashTest.py
+++ hashTest.py
@@ -1,20 +1,35 @@
 import math
 
 def hash(id, tableSize):
-        square = pow(id, 2)
-        counter = 0
-        middle = ""
-        for i in str(square):
-            if(counter == str(square) / 2):
-                middle += i
-            counter += 1
-        return float(middle) % tableSize
+    square = pow(id, 2)
+    counter = 0
+    middle = ''
+    squared = str(square)
+    distFromMiddle = int(math.log(tableSize, 10))
+
+    #print(square)
+    #print(squared)
+
+    middleIndex = int((len(squared) / 2))
+    fwd = middleIndex - distFromMiddle + 1
+    bwd = middleIndex + distFromMiddle
+
+    middleNums = str(int(squared[fwd:bwd]))
+    #print(middleNums)
+    #middle = int(middle) % tableSize
+    #print(middle)
+
+    #return middle
 
 
-size = 10
-id = 44
+    return int(middleNums) % tableSize
 
-for i in range (10):
-	print("testing github")
+
+"""THIS IS A TEST AREA """
+
+size = 100
+id = 144
 
 print(hash(id, size))
+
+#print(math.log(100, 10))
